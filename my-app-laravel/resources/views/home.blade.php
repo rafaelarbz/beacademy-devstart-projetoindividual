9e203566ef6848c34f3f84b82b7977e0cc9e295d
Rafaela Rabelo <rafaelarabeloeng@gmail.com>
2022-07-13 15:48:24 -0300

    ajustes routes

--- my-app-laravel/resources/views/home.blade.php
+++ my-app-laravel/resources/views/home.blade.php
@@ -2,13 +2,6 @@
 @section('title', 'Coffee - Home')
 @section('body')
 
-    @if($errors->any())
-    <div class="alert alert-danger" role="alert">
-        @foreach ($errors->all(); as $error)
-        {{ $error }}<br>
-        @endforeach
-    </div>
-    @endif
     <img src="{{ asset('storage/home/coffee.gif')}}" class="mx-auto d-block" alt="Home Image" width="40%">
 
 @endsection
